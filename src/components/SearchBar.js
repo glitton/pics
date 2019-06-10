7f2283113af02ebe082fad06d71f0ea1bea3748a
Generosa Litton <glcodeworks@gmail.com>
2019-06-09 22:35:01 -0700

    event handlers fixed

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,19 +2,25 @@ import React from "react";
 
 class SearchBar extends React.Component {
   state = {
-    term: ""
+    term: " "
+  };
+
+  onFormSubmit = evt => {
+    evt.preventDefault();
+
+    console.log(this.state.term);
   };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
               type="text"
               value={this.state.term}
-              onChange={e => this.setState({ term: e.target.value })}
+              onChange={evt => this.setState({ term: evt.target.value })}
             />
           </div>
         </form>
