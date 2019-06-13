f0f0144a4c9a6479c63b65add83dd64f16f7a569
Generosa Litton <glcodeworks@gmail.com>
2019-06-13 15:25:47 -0700

    added axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "20px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID a99cb25ac35e99ac574cbe761886959945094242db39d4e41528408284c9075f"
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "20px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,19 +4,17 @@ class SearchBar extends React.Component {
   state = {
     term: " "
   };
-  //  Do this instead of binding
-
+  //  Default way, Do this instead of binding
   onFormSubmit = evt => {
     evt.preventDefault();
-
-    console.log(this.state.term);
+    this.props.onSubmit(this.state.term);
   };
 
   render() {
     return (
       <div className="ui segment">
         {/* Another way would be to add the arrow to onFormSubmit and invoke it */}
-        {/* <form onSubmit={() => this.onFormSubmit()} className="ui form" /> */}
+        {/* <form onSubmit={(evt) => this.onFormSubmit(evt)} className="ui form" /> */}
         <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
