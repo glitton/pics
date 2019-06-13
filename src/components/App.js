ea011d80af3e2b09bf467d858439ad999ec22d29
Generosa Litton <glcodeworks@gmail.com>
2019-06-13 15:34:14 -0700

    added async await for axios request

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,18 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization:
           "Client-ID a99cb25ac35e99ac574cbe761886959945094242db39d4e41528408284c9075f"
       }
     });
+    console.log(
+      "these are the results ",
+      response.data.results[0].urls.regular
+    );
   }
 
   render() {
