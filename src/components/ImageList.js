86c0638b9b5195e62fe97008d978aacf37df52be
Generosa Litton <glcodeworks@gmail.com>
2019-06-13 18:11:45 -0700

    added Image list and mapped through them to render

---
+++ src/components/ImageList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ImageList = props => {
+  const images = props.images.map((image, index) => {
+    return <img alt="car" key={index} src={image.urls.regular} />;
+  });
+  return <div>{images}</div>;
+};
+
+export default ImageList;
